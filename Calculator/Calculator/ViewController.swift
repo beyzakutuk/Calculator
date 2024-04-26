940c80b42cba582d8e3e608905ceb3019c1b341a
Beyza Kütük <beyzakutuk55@gmail.com>
2024-04-26 18:58:44 +0300

    Add project

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Beyza Kütük on 26.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
