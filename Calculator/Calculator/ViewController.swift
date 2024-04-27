55dc39ab49ad59fced9af9b252247b406f7a0035
Beyza Kütük <beyzakutuk55@gmail.com>
2024-04-27 10:54:15 +0300

    Connect views

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,11 +9,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var screenLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func zeroButton(_ sender: Any) {
+    }
+    
+    @IBAction func oneButton(_ sender: Any) {
+    }
+    
+    @IBAction func twoButton(_ sender: Any) {
+    }
+    
+    @IBAction func threeButton(_ sender: Any) {
+    }
+    
+    @IBAction func fourButton(_ sender: Any) {
+    }
+    
+    @IBAction func fiveButton(_ sender: Any) {
+    }
+    
+    @IBAction func sixButton(_ sender: Any) {
+    }
+    
+    @IBAction func sevenButton(_ sender: Any) {
+    }
+    
+    @IBAction func eightButton(_ sender: Any) {
+    }
+    
+    @IBAction func nineButton(_ sender: Any) {
+    }
+    
+    @IBAction func acButton(_ sender: Any) {
+    }
+    
+    @IBAction func signButton(_ sender: Any) {
+    }
+    
+    @IBAction func percentButton(_ sender: Any) {
+    }
+    
+    @IBAction func divisionButton(_ sender: Any) {
+    }
+    
+    @IBAction func multiplicationButton(_ sender: Any) {
+    }
+    
+    @IBAction func subtractionButton(_ sender: Any) {
+    }
+    
+    @IBAction func additionButton(_ sender: Any) {
+    }
+    
+    @IBAction func equalButton(_ sender: Any) {
+    }
+    
 }
 
